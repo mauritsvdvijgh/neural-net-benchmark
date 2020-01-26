67545d24d4a950dedcd4fe021fc3f694b1463438
Maurits van der Vijgh <maurits@elnino.tech>
2020-01-27 00:57:20 +0100

    Fix docker build output to stdout issues

--- post_process.sh
+++ post_process.sh
@@ -1,11 +1,13 @@
 echo "Booting up the orca"
 docker-compose up -d orca
 sleep 2
+echo "Building post-process container image"
+docker-compose build -q post-process
 echo "Generating prediction table data"
-cat results/predict/*.json | docker-compose run post-process prediction_time.py > results/prediction-table.txt
+cat results/predict/*.json | docker-compose run --no-deps --rm  post-process prediction_time.py > results/prediction-table.txt
 echo "Generating batch throughput latency scatter plot"
-cat results/batch/*.json | docker-compose run post-process plot_throughput_latency.py > results/batch_throughput_latency.pdf
+cat results/batch/*.json | docker-compose run --rm post-process plot_throughput_latency.py > results/batch_throughput_latency.pdf
 echo "Generating single throughput latency scatter plot"
-cat results/single/*.json | docker-compose run post-process plot_throughput_latency.py > results/single_throughput_latency.pdf
-echo "Cleaning up the orca"
+cat results/single/*.json | docker-compose run --rm post-process plot_throughput_latency.py > results/single_throughput_latency.pdf
+echo "Killing the orca"
 docker-compose rm -f orca
