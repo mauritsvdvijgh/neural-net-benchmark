aedfa8ebaee6c3ecfed9dceb0e68be1b46cf9e41
Maurits van der Vijgh <maurits@elnino.tech>
2020-01-26 20:40:55 +0100

    refactoring, cleanup, add post processing scripts

--- benchmark/main.py
+++ benchmark/main.py
@@ -1,14 +1,46 @@
-from argparse import ArgumentParser
+# -*- coding: utf-8 -*-
+import inspect
+import json
 import logging
-import ujson as json
-import time
+import os
 import pickle
-import requests
+import resource
+import time
+from argparse import ArgumentParser
+
+import models
+import numpy as np
 
 
-AVAILABLE_FRAMEWORKS = ["scikit", "tensorflow", "cntk", "theano", "mxnet", "pytorch"]
-AVAILABLE_DATASETS = ["mnist"]
-AVAILABLE_MODES = ["http", "direct", "celery"]
+AVAILABLE_MODELS = {
+    model[0].lower(): model[1] for model in inspect.getmembers(models, inspect.isclass)
+}
+MODEL_CLASS = AVAILABLE_MODELS[os.environ.get("FRAMEWORK", "scikit")]
+
+# init model with http put
+model = None
+
+
+def init(hidden_layer_sizes, classes, input_dim, batch_size):
+    global model
+    model = MODEL_CLASS(hidden_layer_sizes, classes, input_dim, batch_size=batch_size)
+
+
+def train(samples, labels):
+    start = time.time()
+    model.train(samples, labels)
+    end = time.time()
+    return {
+        "time": end - start,
+        "mem": resource.getrusage(resource.RUSAGE_SELF).ru_maxrss,
+    }
+
+
+def predict(samples):
+    start = time.time()
+    predictions = model.predict(samples)
+    end = time.time()
+    return {"classes": predictions.tolist(), "time": end - start}
 
 
 def load_mnist():
@@ -28,67 +60,11 @@ def get_accuracy(answers, predictions):
     return right / total
 
 
-def run_benchmark_http(framework, data, epochs=1, split=1):
-    x_train, y_train, x_test, y_test = data
-    url = "http://{}:8000".format(framework)
-    test_data = json.dumps({"samples": x_test.tolist()})
-    step_size = len(x_train) // split
-    stats = {"step_size": step_size, "framework": framework, "epochs": {}}
-    model_config = {"hidden_layer_sizes": (100, ), "classes" : [0,1,2,3,4,5,6,7,8,9], "input_dim" : 784}
-    requests.put(url, json=model_config)
-
-    for epoch in range(epochs):
-        stats["epochs"][epoch] = {"mem": [], "time": [], "accuracy": []}
-        epoch_stats = stats["epochs"][epoch]
-        for step in range(0, len(x_train), step_size):
-            train_data = json.dumps(
-                {
-                    "samples": [x_train[step : step + step_size].tolist()],
-                    "classes": y_train[step : step + step_size].tolist(),
-                }
-            )
-            train_response = requests.post(url, data=train_data).json()
-            test_response = requests.get(url, data=test_data).json()
-            accuracy = get_accuracy(y_test, test_response["classes"])
-            epoch_stats["time"].append(train_response["time"])
-            epoch_stats["mem"].append(train_response["mem"])
-            epoch_stats["accuracy"].append(accuracy)
-    return stats
-
-
-def run_benchmark_direct(data, epochs=1, split=1):
-    x_train, y_train, x_test, y_test = data
-    url = "http://{}:8000".format(framework)
-    test_data = json.dumps({"samples": x_test.tolist()})
-    step_size = len(x_train) // split
-    stats = {"step_size": step_size, "framework": framework, "epochs": {}}
-    model_config = {"hidden_layer_sizes": (100, ), "classes" : [0,1,2,3,4,5,6,7,8,9], "input_dim" : 784}
-    requests.put(url, json=model_config)
-
-    for epoch in range(epochs):
-        stats["epochs"][epoch] = {"mem": [], "time": [], "accuracy": []}
-        epoch_stats = stats["epochs"][epoch]
-        for step in range(0, len(x_train), step_size):
-            train_data = json.dumps(
-                {
-                    "samples": [x_train[step : step + step_size].tolist()],
-                    "classes": y_train[step : step + step_size].tolist(),
-                }
-            )
-            train_response = requests.post(url, data=train_data).json()
-            test_response = requests.get(url, data=test_data).json()
-            accuracy = get_accuracy(y_test, test_response["classes"])
-            epoch_stats["time"].append(train_response["time"])
-            epoch_stats["mem"].append(train_response["mem"])
-            epoch_stats["accuracy"].append(accuracy)
-    return stats
-
-
 def summarize(stats_run):
     return "{} | epochs: {} | batch_size: {} | training time: {} | accuracy: {}".format(
         stats_run["framework"],
         len(stats_run["epochs"]),
-        stats_run["step_size"],
+        stats_run["batch_size"],
         sum(
             [
                 time
@@ -96,55 +72,71 @@ def summarize(stats_run):
                 for time in stats_run["epochs"][epoch_stats]["time"]
             ]
         ),
-        stats_run["epochs"][max(stats_run["epochs"])]["accuracy"][-1],
+        stats_run["epochs"][max(stats_run["epochs"])]["accuracy"],
     )
 
 
-def main(frameworks, datasets, epochs, splits):
+def run_benchmark_direct(data, epochs=1, batch_size=60000):
+    x_train, y_train, x_test, y_test = data
+    init((200, 200), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 784, min(200, batch_size))
+    stats = {
+        "batch_size": batch_size,
+        "framework": os.environ.get(
+            "KERAS_BACKEND", os.environ.get("FRAMEWORK", "scikit")
+        ),
+        "epochs": {},
+    }
+    for epoch in range(epochs):
+        stats["epochs"][epoch] = {"mem": [], "time": []}
+        epoch_stats = stats["epochs"][epoch]
+
+        for batch_offset in range(0, len(x_train) - 1, batch_size):
+            train_feedback = train(
+                x_train[batch_offset : batch_offset + batch_size],
+                y_train[batch_offset : batch_offset + batch_size],
+            )
+            epoch_stats["time"].append(train_feedback["time"])
+            epoch_stats["mem"].append(train_feedback["mem"])
+
+        test_feedback = predict(x_test)
+        accuracy = get_accuracy(y_test, test_feedback["classes"])
+        epoch_stats["accuracy"] = accuracy
+    return stats
+
+
+def run_benchmark_predict(data):
+    x_train, y_train, x_test, y_test = data
     stats = []
-    for data in datasets:
-        for framework in frameworks:
-            for split in splits:
-                data = load_mnist()
-                stats_run = run_benchmark(framework, data, epochs, split)
-                stats.append(stats_run)
-                print(summarize(stats_run))
-    print(stats)
+    for i in range(0, len(x_test), 1):
+        start = time.time()
+        predict(x_test[i : i + 1])
+        end = time.time()
+        stats.append(end - start)
+    return stats
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument(
-        "--mode",
-        dest="selected_mode",
-        type: str,
-        default='http'
-        help="benchmark mode",
-    )
-
-    parser.add_argument(
-        "--frameworks",
-        dest="selected_frameworks",
-        type=lambda s: [str(item) for item in s.split(",")],
-        default=AVAILABLE_FRAMEWORKS,
-        help="frameworks to benchmark",
-    )
+    parser.add_argument("--predict", dest="predict", action="store_true")
     parser.add_argument(
         "--epochs", dest="epochs", type=int, default=1, help="epochs to train"
     )
     parser.add_argument(
-        "--splits",
-        dest="splits",
-        type=lambda s: [int(item) for item in s.split(",")],
-        default=[1],
-        help="number of parts to split the dataset in before training",
-    )
-    parser.add_argument(
-        "--datasets",
-        dest="selected_datasets",
-        type=lambda s: [str(item) for item in s.split(",")],
-        default=AVAILABLE_DATASETS,
-        help="datasets to benchmark",
+        "--batch_size",
+        dest="selected_batch_size",
+        type=int,
+        default=1,
+        help="batch size",
     )
     args = parser.parse_args()
-    main(args.selected_frameworks, args.selected_datasets, args.epochs, args.splits)
+
+    data = load_mnist()
+    stats = run_benchmark_direct(data, args.epochs, args.selected_batch_size)
+    if args.predict:
+        stats = {
+            "times": run_benchmark_predict(data),
+            "framework": os.environ.get(
+                "KERAS_BACKEND", os.environ.get("FRAMEWORK", "scikit")
+            ),
+        }
+    print(json.dumps(stats))
